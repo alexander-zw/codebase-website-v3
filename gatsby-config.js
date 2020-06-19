4891f9587689ab14983e18ccc24820cf813b1247
Kelvin Jue <kelvinjue@berkeley.edu>
2020-06-19 18:48:59 -0500

    added basic menu navigation for all pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,30 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `codebase`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@codebase-berkeley`,
+    menuLinks: [
+      {
+        name: `home`,
+        link: `/`,
+      },
+      {
+        name: `about`,
+        link: `/about`,
+      },
+      {
+        name: `projects`,
+        link: `/projects`,
+      },
+      {
+        name: `contact`,
+        link: `/contact`,
+      },
+      {
+        name: `apply`,
+        link: `/apply`,
+      },
+    ],
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,7 +46,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/codebase-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,19 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1>Hello people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    {/* <Link to="/about/">Go to about page</Link> <br />
+    <Link to="/projects/">Go to projects page</Link> <br />
+    <Link to="/contact/">Go to contact page</Link> <br />
+    <Link to="/apply/">Go to apply page</Link> <br /> */}
   </Layout>
 )
 
