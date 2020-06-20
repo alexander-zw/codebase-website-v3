e7338f322720ab7d21891c837d031d5a0e11df81
Kelvin Jue <kelvinjue@berkeley.edu>
2020-06-19 22:57:21 -0500

    oops missed a prop

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,7 @@ import SEO from "../components/seo"
 const About = ({ location }) => {
   console.log(location)
   return (
-    <Layout pathname={location.pathname}>
+    <Layout curPath={location.pathname}>
       <SEO title="About" />
       <h1>About</h1>
     </Layout>
