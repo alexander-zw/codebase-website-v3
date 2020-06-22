1e6f8687b6c1306e5a80c1e749bb6f80c1149f4b
Codebase <codebaseberkeley@gmail.com>
2020-06-22 05:01:05 +0000

    Finish header

--- src/components/home/home-header.js
+++ src/components/home/home-header.js
@@ -1,7 +1,8 @@
 import React from "react"
+import TextLoop from "react-text-loop"
 
 import "../../styles/home-header.css"
-import homeHeaderImage from "../../images/home-header-image.png"
+import homeHeaderImage from "../../images/home-header-animation.gif"
 
 const HomeHeader = () => {
   return (
@@ -9,7 +10,16 @@ const HomeHeader = () => {
       <div className="container">
         <div className="row cb-home-header-row">
           <div className="col-md-4">
-            <h1 className="cb-home-header-title">We are a group of mentors.</h1>{" "}
+            <h1 className="cb-home-header-title">
+              We are a community of{" "}
+              <TextLoop
+                children={["students", "coders", "mentors", "friends"]}
+                springConfig={{ stiffness: 180, damping: 15 }}
+                interval={[3000, 3000, 3000, 3750]}
+                adjustingSpeed={200}
+              />
+              .
+            </h1>{" "}
             <p className="cb-home-header-subtitle">
               Codebase is a UC Berkeley student organization that builds
               software projects for high growth tech companies.
