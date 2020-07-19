7344a0956851828e9373e36fd293e15aa141423c
Codebase <codebaseberkeley@gmail.com>
2020-07-19 00:45:42 +0000

    Revamp projects

--- src/components/home/home-projects.js
+++ src/components/home/home-projects.js
@@ -8,32 +8,34 @@ import "../../styles/layout.css"
 
 const HomeProjects = () => {
   return (
-    <Container className="cb-home-projects">
-      <h1 className="cb-home-projects-title">THIS SEMESTER'S PROJECTS</h1>
+    <div className="cb-home-projects-container">
+      <Container className="cb-home-projects">
+        <h1 className="cb-home-projects-title">THIS SEMESTER'S PROJECTS</h1>
 
-      <ProjectsSection
-        heading="Client projects"
-        description={
-          "Our client teams work with industry partners to build products " +
-          "ranging from full stack web development to machine learning."
-        }
-        client={true}
-      />
+        <ProjectsSection
+          heading="Client Projects"
+          description={
+            "Our client teams work with industry partners to build products " +
+            "ranging from full stack web development to machine learning."
+          }
+          client={true}
+        />
 
-      <ProjectsSection
-        heading="Mentored project"
-        description={
-          "Our mentored team focuses on learning the essentials of " +
-          "software development and simultaneously develops an full-stack " +
-          "web application for a non-profit organization."
-        }
-        client={false}
-      />
+        <ProjectsSection
+          heading="Mentored Project"
+          description={
+            "Our mentored team focuses on learning the essentials of " +
+            "software development and simultaneously develops an full-stack " +
+            "web application for a non-profit organization."
+          }
+          client={false}
+        />
 
-      <Link className="cb-orange-link" to="projects">
-        Check out how each project works →
-      </Link>
-    </Container>
+        <Link className="cb-blue-link" to="projects">
+          Check out how each project works →
+        </Link>
+      </Container>
+    </div>
   )
 }
 
